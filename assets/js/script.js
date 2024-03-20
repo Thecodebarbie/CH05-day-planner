b682ef20fb8557ee6528521ae199a672a7bbca16
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-03-20 13:10:58 -0400

    Created a function to handle save button clicks, ensuring accurate selection of elements using event.target. Incorporated conditional checks to precisely identify the clicked element. Finally, set up an event listener on save buttons to enable smooth interaction with the functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,51 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  
+
   var currentDate = dayjs()
   var currentHour = dayjs().hour() // 0 to 23 military time
   console.log(currentHour)
-var currentDayEl = $('#currentDay')
-  
-currentDayEl.text(currentDate.format("dddd, MMMM DD"))
+  var currentDayEl = $('#currentDay')
 
-// time block is from 9 to 17
-for(var i = 9; i < 18; i++){
-    var parentId =$("#hour-" + i) //parent selector
-     var textarea = parentId.children('textarea')
-    if(i===currentHour){ // for present
+  currentDayEl.text(currentDate.format("dddd, MMMM DD"))
+
+  // time block is from 9 to 17
+  for (var i = 9; i < 18; i++) {
+    var parentId = $("#hour-" + i) //parent selector
+    var textarea = parentId.children('textarea')
+    if (i === currentHour) { // for present
       textarea.addClass('present')
     }
-    else if(i < currentHour){ // for past
+    else if (i < currentHour) { // for past
       textarea.addClass('past')
-    }else{
+    } else {
       // i > currentHour // for future
       textarea.addClass('future')
     }
-}
+  }
 
+  var saveBtnEl = $('.saveBtn')
 
+  function handleSaveEvent(event) {
+    var textareaEl
+    if ($(event.target).attr('class') === 'fas fa-save') {
+      // i element <i class="fas fa-save" aria-hidden="true"></i>
+      var iEl = $('event.target') //event target is current
+      textareaEl = iEl.parent().siblings(textarea)
+      console.log(iEl, "current button")
+      console.log(textareaEl, "sibling of button")
+    }
+    else {
+      // button <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+    }
 
+    var buttonEl = $('event.target') //event target is current
+    textareaEl = buttonEl.siblings(textarea)
+    console.log(buttonEl, "current button")
+    console.log(textareaEl, "sibling of button")
+  }
+
+  saveBtnEl.on('click', handleSaveEvent)
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
